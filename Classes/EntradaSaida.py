5a2cb84a7e698c10e1932a82626b9f1e173e2066
Valdimir Barros <valdimirbarrosti@gmail.com>
2020-03-11 01:05:44 -0300

    primeiras implementações  no front end da aplicação

--- Classes/EntradaSaida.py
+++ Classes/EntradaSaida.py
@@ -1,16 +1,23 @@
 #CLASSE QUE REPRESENTA OS DISPOSITIVOS DE ENTRADA E SAÍDA DE DADOS (INPUT OUTPUT SYSTEM) DO IAS#
 import webbrowser
+import json
 
 
 class EntradaSaida:
     def inicializar(self):
         print('Dispositivos de Entrada e Saída incializados...')
-        webbrowser.open("C:/wamp64/www/UFMA/archias/View/archias.html", new=1)
+        webbrowser.open("C:/wamp64/www/UFMA/archias/Exibicao/archias.html", new=1)
 
     def exibirResultado(self):
         print('Exibindo resultado do programa no navegador padrão instalado...')
+        f = open('obj.json', 'w')
+        f.write('This is a test\n')
+        f.close()
+        # objJson = json.dumps([1, 'simple', 'list'])
+        # json.dump(json, arquivo)
+
         webbrowser.open(
-            "C:/wamp64/www/UFMA/archias/View/resultado.html", new=1)
+            "C:/wamp64/www/UFMA/archias/Exibicao/resultado.html", new=1)
 
     def exibirCaracteristicasComputador(self, quantidadeEnderecosMemoria, quantidadeBitsPalavra):
         print('Este Computador Possui ' +
@@ -19,4 +26,5 @@ class EntradaSaida:
               str(quantidadeBitsPalavra) + ' bits')
 
 
-## NOTA!! REFLETIR SE VALE A PENA OU NAO PASSAR A ABERTURA DO BROWSER NO METODO INICILIZAR PARA UMA FUNÇÃO MAIS ATRAENTE...
+# NOTA!!1 REFLETIR SE VALE A PENA OU NAO PASSAR A ABERTURA DO BROWSER NO METODO INICILIZAR PARA UMA FUNÇÃO MAIS ATRAENTE...
+# NOTA!!2 BUSCAR UMA FORMA PARA GRAVAR UM ARQUIVO JSON NO DIRETÓRIO (SAIDA DE RESULTADOS )PARA SER LIDO PELO NAVEGADOR TALVEZ UMA RESPOSTA AO CLIENTE VOU VER AINDA COMO FACO NAO QQUERO USAR NADA DE HTTP MAS FAZER OQUE NÉ MANO
---
+++ principal.py
@@ -0,0 +1,11 @@
+#ARQUIVO PRINCIPAL DO PROGRAMA#
+from Classes.Computador import Computador
+
+computador = Computador()
+computador.ligar()
+computador.exibirCaracteristicas()
+#computador.carregarPrograma()
+computador.exibirResultado()
+computador.desligar()
+
+#NOTA!! CONTINUAR COM AS OUTRAS PARTES DA APLICAÇÃO E CHAMAR AQUI QUANDO NECESSÁRIO...
